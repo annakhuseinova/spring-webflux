06526dd012b44b61fee2e954c78bfad9aeef55c2
Anna Khuseinova <annakhuseinova@yandex.ru>
2021-08-07 16:16:24 +0300

    More code

---
+++ src/main/java/com/annakhuseinova/springwebflux/service/MathService.java
@@ -0,0 +1,25 @@
+package com.annakhuseinova.springwebflux.service;
+
+import com.annakhuseinova.springwebflux.SleepUtil;
+import com.annakhuseinova.springwebflux.dto.Response;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+@Service
+public class MathService {
+
+    public Response findSquare(int input){
+        return new Response(input * input);
+    }
+
+    public List<Response> multiplicationTable(int input){
+        return IntStream.rangeClosed(1, 10)
+                .peek(i -> SleepUtil.sleepSeconds(1))
+                .peek(i -> System.out.println("math-service processing :" + i))
+                .mapToObj(item -> new Response(item * input))
+                .collect(Collectors.toList());
+    }
+}
