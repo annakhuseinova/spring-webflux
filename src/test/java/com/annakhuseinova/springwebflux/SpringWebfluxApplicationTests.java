1354a2189af95f3bfa8b76b3e9b60dffcb4b397d
Anna Khuseinova <annakhuseinova@yandex.ru>
2021-08-07 14:29:36 +0300

    Initial commit

---
+++ src/test/java/com/annakhuseinova/springwebflux/SpringWebfluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.annakhuseinova.springwebflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebfluxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
